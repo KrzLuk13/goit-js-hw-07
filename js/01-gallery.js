21259a619ea61943f43a2bdf76f37e025d0ee973
KrzLuk13 <k13.lukasik@gmail.com>
2024-05-04 18:08:04 +0200

    1

---
+++ js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from "./gallery-items.js";
+const gallery = document.querySelector(".gallery");
+
+// Funkcja do dodawania elementów galerii
+function addGalleryItems() {
+  galleryItems.forEach((item) => {
+    const li = document.createElement("li");
+    li.classList.add("gallery__item");
+
+    const a = document.createElement("a");
+    a.classList.add("gallery__link");
+    a.setAttribute("href", item.original);
+
+    const img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.setAttribute("src", item.preview);
+    img.setAttribute("alt", item.description);
+    img.setAttribute("data-source", item.original);
+
+    a.appendChild(img);
+    li.appendChild(a);
+    gallery.appendChild(li);
+  });
+}
+
+// Dodanie elementów galerii po załadowaniu zawartości DOM
+document.addEventListener("DOMContentLoaded", function () {
+  addGalleryItems();
+});
+
+// Obsługa kliknięcia na obrazek
+gallery.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (event.target.nodeName === "IMG") {
+    const src = event.target.dataset.source;
+    const alt = event.target.getAttribute("alt");
+
+    const instance = basicLightbox.create(`
+      <img src="${src}">
+    `);
+
+    instance.show();
+
+    console.log("Image source:", src);
+    console.log("Image alt:", alt);
+  }
+});
+
+console.log(galleryItems);
---
+++ js/02-lightbox.js
@@ -0,0 +1,36 @@
+import { galleryItems } from "./gallery-items.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const gallery = document.querySelector(".gallery");
+
+  galleryItems.forEach((item) => {
+    const li = document.createElement("li");
+    li.classList.add("gallery__item");
+
+    const a = document.createElement("a");
+    a.classList.add("gallery__link");
+    a.setAttribute("href", item.original);
+
+    const img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.setAttribute("src", item.preview);
+    img.setAttribute("alt", item.description);
+    img.setAttribute("data-source", item.original);
+    img.setAttribute("title", item.description); // Dodanie opisu jako tytułu obrazka
+
+    a.appendChild(img);
+    li.appendChild(a);
+    gallery.appendChild(li);
+  });
+
+  gallery.addEventListener("click", function (event) {
+    event.preventDefault();
+  });
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true, // Włączenie wyświetlania podpisów
+    captionDelay: 250, // Ustawienie opóźnienia w wyświetlaniu podpisu
+  });
+
+  console.log(galleryItems);
+});
