152708db9a3f2507b75087842aa2fedfc120d65e
KrzLuk13 <k13.lukasik@gmail.com>
2024-05-04 18:10:16 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -32,5 +32,5 @@ document.addEventListener("DOMContentLoaded", function () {
     captionDelay: 250, // Ustawienie opóźnienia w wyświetlaniu podpisu
   });
 
-  console.log(galleryItems);
+  console.log(gallery Items);
 });
